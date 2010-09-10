576e40c3cbf7d2e7d91ee0de6356bb9402247ca9
hideto.bj@gmail.com <Hideto@pek-fwm-dhcp-c115.dev.fwmrm.net>
2010-09-10 16:38:29 +0800

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Sh800::Application.config.secret_token = '22725d23c447b68b60090e3368813d84323f4a5564fa099d4123ca27e3d9c6f87e05fac9836fa32bbee6a1ac75ec21735626d07c87b3ac6555ea8f8daae3f212'
