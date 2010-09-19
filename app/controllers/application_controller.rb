29a4de48c7168287d0da3257263dc244ec265057
hideto.bj@gmail.com <Hideto@Hideto.local>
2010-09-20 04:47:51 +0800

    ip redirect

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,25 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
   
+  before_filter :redirect_by_ip
   before_filter :set_charset
   before_filter :configure_charsets
+  
+  def redirect_by_ip
+    return if request.fullpath == "/city/list"
+    if request.subdomain == "" || request.subdomain == "www"
+      ip = request.remote_ip
+      ct = "beijing"
+      r = IP_LIB.find ip
+      addr = r.country.to_s
+      ALL_CITIES.each do |c|
+        if addr.include?(c.name)
+          ct = c.slug
+        end
+      end
+      redirect_to "http://" + ct + "." + SITE_DOMAIN + request.fullpath
+    end
+  end
 
   def set_charset
     headers["Content-Type"] = "text/html; charset=utf-8"
--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,7 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the rails application
 Sh800::Application.initialize!
+
+SITE_DOMAIN = "800shenghuo.com"
+IP_LIB = QQWry::QQWryFile.new
+ALL_CITIES = City.find(:all)
